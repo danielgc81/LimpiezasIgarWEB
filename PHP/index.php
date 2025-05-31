ad5e660c717a1f513b855c8e9db809630d3d64f3
Daniel Garrido <danielgc81@educastur.es>
2025-05-31 16:37:46 +0200

    update

--- PHP/index.php
+++ PHP/index.php
@@ -1,3 +1,20 @@
+<?php
 
+   $usuarios_web = [
+      "Gonzalo" => "1234",
+      "Garrido" => "1234"
+   ];
+
+   $username = $_POST['username'];
+   $password = $_POST['password'];
+
+   $session_correcta = false;
+
+   foreach ($usuarios_web as $usuario => $pass) {
+      if ($username == $usuario && $password == $pass) {
+         $session_correcta = true;
+      }
+   }
+?>
 
 
